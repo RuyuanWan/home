480e9c6dd4ec5b953856d6370eb51429d281e462
RuyuanWan <30328069+RuyuanWan@users.noreply.github.com>
2020-11-29 04:03:27 -0600

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -17,7 +17,7 @@ const mainBody = {
     },
     {
       image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
+      url: "https://www.facebook.com/RuyuanWan",
     },
     {
       image: "fa-instagram",
@@ -66,7 +66,7 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
+  gitHubUsername: "RuyuanWan", //i.e."johnDoe12Gh"
   reposLength: 4,
   specificRepos: [],
 };
