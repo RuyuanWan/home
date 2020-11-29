0ad691e16b63785ce894e70491fde53f76464bd4
Ruyuan Wan <496286316@qq.com>
2020-11-29 04:20:06 -0600

    Updates

---
+++ precache-manifest.bce27a520de4158f5eb04331bf10d9f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a879ea64be284fbd143ad876e7e1293",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "ec5c7d8e9c1b511d36f1",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5c7d8e9c1b511d36f1",
+    "url": "/home/static/js/main.c4a935db.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "7071f8d50a312d7ee5392c79ab1b80de",
+    "url": "/home/static/media/resume.7071f8d5.pdf"
+  },
+  {
+    "revision": "7f21e4b74a459d18b79cb63ed317ed85",
+    "url": "/home/static/media/ruyuanwan.7f21e4b7.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.543a270ec46c2784cf6fcb4bbfe4845f.js"
+  "/home/precache-manifest.bce27a520de4158f5eb04331bf10d9f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
